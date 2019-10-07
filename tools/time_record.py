e82ac18ec1cfc49e556e7dab29c51513f999efe6
tulip4attoo <huylinh.nguyen@outlook.com>
2019-10-07 16:00:22 +0700

    update time_record file

---
+++ tools/time_record.py
@@ -0,0 +1,5 @@
+import tqdm
+import time
+
+for i in tqdm.tqdm(range(1000)):
+    time.sleep(0.01)
--- tools/ubuntu_video_cut.py
+++ tools/ubuntu_video_cut.py
@@ -3,23 +3,27 @@ import cv2
 FRAMERATE = 8
 
 video_capture = cv2.VideoCapture("./vids/vid0_full.mp4")
-fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
 out = cv2.VideoWriter("./vids/vid0.avi", fourcc, FRAMERATE, (1920, 1080))
 video_capture.set(cv2.CAP_PROP_POS_FRAMES, FRAMERATE*47)
 count = 0
 while(True):
     # Capture frame-by-frame
     ret, frame = video_capture.read()
-    if(frame is None): break #check for empty frames
+    if(frame is None):
+        break  # check for empty frames
     # img_copy = utils.find_keypoints(frame)
 
     out.write(frame)
-    cv2.imshow('Original', frame) #show on window
-    if cv2.waitKey(1) & 0xFF == ord('q'): break #Exit when Q is pressed
+    cv2.imshow('Original', frame)  # show on window
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break  # Exit when Q is pressed
     count += 1
     if count == FRAMERATE * 80:
         break
 
-#Release the camera
+
+# Release the camera
+
 video_capture.release()
 print("Bye...")
